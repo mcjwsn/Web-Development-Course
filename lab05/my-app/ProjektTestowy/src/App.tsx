7cfaaca19a7bfb8702e83cef32e104199204dd25
mcjwsn <maciek.wisniewski04@gmail.com>
2025-01-09 18:55:18 +0100

    cwiczenia testwowe
    przykladowe cwiczenia z pliku

--- lab05/my-app/ProjektTestowy/src/App.tsx
+++ lab05/my-app/ProjektTestowy/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function Welcome(props: { name: string }) {
-  return <h1>Hello, {props.name}</h1>;
+function Welcome(props: { value: number, name : string }) {
+  const gender = props.name.endsWith('a') ? 'K': 'M';
+  return <h1>Mam {props.value} lat i nazywam się {props.name}, moja pleć to {gender}</h1>;
 }
-
+function GoodBye(props: {name : string}){
+  return <h2> Goodbye {props.name} :D</h2>;
+}
 function App() {
   return (
     <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+      <Welcome value = {13} name = "Pawel" />
+      <Welcome value = {14} name = "Lara" />
+      <br></br>
+      <GoodBye name = 'Sara'/>
     </div>
   );}
 
