335c07fb11b5781a928a02323fb8dba4008c029c
mcjwsn <maciek.wisniewski04@gmail.com>
2025-01-09 23:09:33 +0100

    zadanie 3

--- lab05/my-app/ProjektTestowy/src/App.tsx
+++ lab05/my-app/ProjektTestowy/src/App.tsx
@@ -4,7 +4,9 @@ import Koszyk from './components/koszyk/Koszyk';
 import NowyKoszyk from './components/koszyk/NowyKoszyk';
 import Licznik from './components/liczniki/Licznik';
 import NowyLicznik from './components/liczniki/NowyLicznik';
-
+import Formularz from './components/formularze/Formularz';
+import Haslo from './components/formularze/Haslo';
+import Logowanie from './components/formularze/Logowanie';
 
 function App() {
   return (
@@ -13,6 +15,9 @@ function App() {
       {/*<NowyKoszyk></NowyKoszyk>*/}
       {/*<Licznik></Licznik>*/}
       {/*<NowyLicznik></NowyLicznik>*/}
+      {/*<Formularz></Formularz>*/}
+      {/*<Haslo></Haslo>*/}
+      {/*<Logowanie></Logowanie>*/}
     </div>
   );}
 
