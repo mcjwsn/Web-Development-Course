ca0cb4be87441a9437d1f9a4d8824fb9b020ff7d
mcjwsn <maciek.wisniewski04@gmail.com>
2025-01-09 18:30:54 +0100

    lab5 start

---
+++ lab05/my-app/ProjektTestowy/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function Welcome(props: { name: string }) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );}
+
+
+export default App;
