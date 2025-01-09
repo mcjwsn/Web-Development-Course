8098b9cd8d743aa9be07d277fcc438add4a72d01
mcjwsn <maciek.wisniewski04@gmail.com>
2025-01-09 19:36:50 +0100

    zad1

--- lab05/my-app/ProjektTestowy/src/App.tsx
+++ lab05/my-app/ProjektTestowy/src/App.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
+import Koszyk from './components/koszyk/Koszyk';
+import NowyKoszyk from './components/koszyk/NowyKoszyk';
 
-function Welcome(props: { value: number, name : string }) {
-  const gender = props.name.endsWith('a') ? 'K': 'M';
-  return <h1>Mam {props.value} lat i nazywam się {props.name}, moja pleć to {gender}</h1>;
-}
-function GoodBye(props: {name : string}){
-  return <h2> Goodbye {props.name} :D</h2>;
-}
+
 function App() {
   return (
     <div>
-      <Welcome value = {13} name = "Pawel" />
-      <Welcome value = {14} name = "Lara" />
-      <br></br>
-      <GoodBye name = 'Sara'/>
+      {/*<Koszyk></Koszyk>*/}
+      {/*<NowyKoszyk></NowyKoszyk>*/}
     </div>
   );}
 
